fcdfcdb71572518630529ab1088b69617a9da4b6
Vishnuworld <vbhandari9561@gmail.com>
2021-09-05 21:15:15 +0530

    added a comment

--- boards/models.py
+++ boards/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
 
+# adding a new line in master branch
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
