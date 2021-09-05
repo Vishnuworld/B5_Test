25cb17b871497bbccbbc685353bbd7fa07de192a
Vishnuworld <vbhandari9561@gmail.com>
2021-09-05 21:24:15 +0530

    added mastermodel

--- boards/models.py
+++ boards/models.py
@@ -54,3 +54,21 @@ class Post(models.Model):
 
 class Test:
     name = models.CharField(max_length=100)
+
+
+# import the standard Django Model
+# from built-in library
+from django.db import models
+
+# declare a new model with a name "GeeksModel"
+class MasterModel(models.Model):
+		# fields of the model
+	title = models.CharField(max_length = 200)
+	description = models.TextField()
+	last_modified = models.DateTimeField(auto_now_add = True)
+	img = models.ImageField(upload_to = "images/")
+
+		# renames the instances of the model
+		# with their title name
+	def __str__(self):
+		return self.title
