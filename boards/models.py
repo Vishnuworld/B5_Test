cfab49ab38ae820faa0c38f57caf8d93f81e5c59
Vishnuworld <vbhandari9561@gmail.com>
2021-09-05 21:17:38 +0530

    added a comment in models.py

--- boards/models.py
+++ boards/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
 
+# added a comment in f1 branch
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
